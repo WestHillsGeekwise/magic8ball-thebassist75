5fc6fda3829fbb53dbfdad87c24d121050b78cb3
Adam Yates <beethoven123456@yahoo.com>
2016-10-10 15:59:52 -0700

    finished magic8

--- js/script.js
+++ js/script.js
@@ -1,1 +1,40 @@
-// add all your js here
+
+// Picture Array
+function picChange(){
+
+var images = ["http://planetoftheweb.com/i/8ball/19.png",
+"http://planetoftheweb.com/i/8ball/18.png",
+"http://planetoftheweb.com/i/8ball/17.png",
+"http://planetoftheweb.com/i/8ball/16.png",
+"http://planetoftheweb.com/i/8ball/15.png",
+"http://planetoftheweb.com/i/8ball/14.png",
+"http://planetoftheweb.com/i/8ball/13.png",
+"http://planetoftheweb.com/i/8ball/12.png",
+"http://planetoftheweb.com/i/8ball/11.png",
+"http://planetoftheweb.com/i/8ball/10.png",
+"http://planetoftheweb.com/i/8ball/9.png",
+"http://planetoftheweb.com/i/8ball/8.png",
+"http://planetoftheweb.com/i/8ball/7.png",
+"http://planetoftheweb.com/i/8ball/6.png",
+"http://planetoftheweb.com/i/8ball/5.png",
+"http://planetoftheweb.com/i/8ball/4.png",
+"http://planetoftheweb.com/i/8ball/3.png",
+"http://planetoftheweb.com/i/8ball/2.png",
+"http://planetoftheweb.com/i/8ball/1.png",
+"http://planetoftheweb.com/i/8ball/0.png"];
+
+  var change = images[Math.floor(Math.random() * images.length)];
+  document.getElementById('randomize').src = change;
+
+}
+
+// Erase Function
+function erase() {
+  document.getElementById('questions').value = "";
+}
+
+// Debug Listeners
+//var x = document.getElementById('clear');
+//var y = document.getElementById('randomize');
+//x.addEventListener('click', erase);
+//y.addEventListener('click', picChange);
